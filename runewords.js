d87a9bb21a272d790805d9a1c0fcbcd0255a2a6b
abickford <atb5498@gmail.com>
2021-03-31 19:26:44 -0400

    updated and fixed offCanvasNav for runewords.html

---
+++ runewords.js
@@ -0,0 +1,14 @@
+function openOffcanvas() {
+    // document.getElementById("myOffcanvas").style.width = "250px";
+    document.getElementById("myOffcanvas").classList.add("offCanvasWidth");
+}
+function openNav3() {
+    document.getElementById("myCanvasNav").style.width = "100%";
+    document.getElementById("myCanvasNav").style.opacity = "0.8";
+}
+function closeOffcanvas() {
+    // document.getElementById("myOffcanvas").style.width = "0%";
+    document.getElementById("myOffcanvas").classList.remove("offCanvasWidth");
+    document.getElementById("myCanvasNav").style.width = "0%";
+    document.getElementById("myCanvasNav").style.opacity = "0";
+}

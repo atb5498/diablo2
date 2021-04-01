d04d98b74bda1b3e5a2a04b624103340d9aaf220
abickford <atb5498@gmail.com>
2021-03-31 23:19:08 -0400

    updated runewords.html

--- runewords.js
+++ runewords.js
@@ -2,10 +2,12 @@ function openOffcanvas() {
     // document.getElementById("myOffcanvas").style.width = "250px";
     document.getElementById("myOffcanvas").classList.add("offCanvasWidth");
 }
+
 function openNav3() {
     document.getElementById("myCanvasNav").style.width = "100%";
     document.getElementById("myCanvasNav").style.opacity = "0.8";
 }
+
 function closeOffcanvas() {
     // document.getElementById("myOffcanvas").style.width = "0%";
     document.getElementById("myOffcanvas").classList.remove("offCanvasWidth");

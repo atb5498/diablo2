617fcffa9bd513c5f79354b0178417a272c3c213
abickford <atb5498@gmail.com>
2021-04-08 23:53:15 -0400

    added more runewords

--- runewords.js
+++ runewords.js
@@ -55,7 +55,7 @@ let runewords = [
             "Freezes Target +3",
             "+10 To Vitality",
             "Replenish Life +8",
-            "1.875-185.625% Extra Gold From Monsters (Based on Character Level)"
+            "1-185% Extra Gold From Monsters (Based on Character Level)"
         ],
         ladder: true
     },
@@ -65,17 +65,17 @@ let runewords = [
         runes: ["Sur", "Cham", "Amn", "Lo"],
         level: 67,
         stats: [
-            "25% Chance To Cast Level 17 Fire Wall When Struck",
-            "Level 16-20 Concentration Aura When Equipped (varies)",
-            "260-300% Bonus To Attack Rating (varies)",
-            "+1-99% Damage To Demons (Based on Character Level)",
-            "Adds 50-280 Lightning Damage",
+            "100% Chance To Cast Level 36 Blaze When You Level-Up",
+            "100% Chance To Cast Level 48 Meteor When You Die",
+            "Level 16 Holy Fire Aura When Equipped",
+            "+33% Increased Attack Speed",
+            "+280-330% Enhanced Damage (varies)",
+            "Ignore Target's Defense",
+            "7% Life Stolen Per Hit",
+            "-20% To Enemy Fire Resistance",
             "20% Deadly Strike",
             "Hit Blinds Target",
-            "Freezes Target +3",
-            "+10 To Vitality",
-            "Replenish Life +8",
-            "1.875-185.625% Extra Gold From Monsters (Based on Character Level)"
+            "Freezes Target +3"
         ],
         ladder: false
     },
@@ -114,7 +114,7 @@ let runewords = [
             "60-70% Chance of Crushing Blow (varies)",
             "Prevent Monster Heal",
             "Hit Blinds Target",
-            "+0.5-49.5% Chance of Getting Magic Items (Based on Character Level)"
+            "+0-49% Chance of Getting Magic Items (Based on Character Level)"
         ],
         ladder: true
     },
@@ -159,6 +159,156 @@ let runewords = [
         ladder: false
     },
     {
+        name: "Phoenix",
+        bases: "4 Socket Weapons/Shields",
+        runes: ["Vex", "Vex", "Lo", "Jah"],
+        level: 65,
+        stats: {
+            weapons: [
+                "100% Chance To Cast level 40 Blaze When You Level-up",
+                "40% Chance To Cast Level 22 Firestorm On Striking",
+                "Level 10-15 Redemption Aura When Equipped (varies)",
+                "+350-400% Enhanced Damage (varies)",
+                "Ignores Target's Defense",
+                "14% Mana Stolen Per Hit",
+                "-28% To Enemy Fire Resistance",
+                "20% Deadly Strike",
+                "+350-400 Defense Vs. Missile (varies)",
+                "+15-21 Fire Absorb (varies)"
+            ],
+            shields: [
+                "100% Chance To Cast level 40 Blaze When You Level-up",
+                "40% Chance To Cast Level 22 Firestorm On Striking",
+                "Level 10-15 Redemption Aura When Equipped (varies)",
+                "+350-400% Enhanced Damage (varies)",
+                "-28% To Enemy Fire Resistance",
+                "+350-400 Defense Vs. Missile (varies)",
+                "+50 To Life",
+                "+5% To Maximum Lightning Resist",
+                "+10% To Maximum Fire Resist",
+                "+15-21 Fire Absorb (varies)"
+            ]
+        },
+        ladder: true
+    },
+    {
+        name: "Brand",
+        bases: "4 Socket Missile Weapons",
+        runes: ["Jah", "Lo", "Mal", "Gul"],
+        level: 65,
+        stats: [
+            "35% Chance To Cast Level 14 Amplify Damage When Struck",
+            "100% Chance To Cast Level 18 Bone Spear On Striking",
+            "Fires Explosive Arrows or Bolts",
+            "+260-340% Enhanced Damage (varies)",
+            "Ignore Target's Defense",
+            "20% Bonus to Attack Rating",
+            "+280-330% Damage To Demons (varies)",
+            "20% Deadly Strike",
+            "Prevent Monster Heal",
+            "Knockback"
+        ],
+        ladder: true
+    },
+    {
+        name: "Faith",
+        bases: "4 Socket Missile Weapons",
+        runes: ["Ohm", "Jah", "Lem", "Eld"],
+        level: 65,
+        stats: [
+            "Level 12-15 Fanaticism Aura When Equipped (varies)",
+            "+1-2 To All Skills (varies)",
+            "+330% Enhanced Damage",
+            "Ignore Target's Defense",
+            "300% Bonus To Attack Rating",
+            "+75% Damage To Undead",
+            "+50 To Attack Rating Against Undead",
+            "+120 Fire Damage",
+            "All Resistances +15",
+            "10% Reanimate As: Returned",
+            "75% Extra Gold From Monsters"
+        ],
+        ladder: true
+    },
+    {
+        name: "Famine",
+        bases: "4 Socket Axes/Hammers",
+        runes: ["Fal", "Ohm", "Ort", "Jah"],
+        level: 65,
+        stats: [
+            "+30% Increased Attack Speed",
+            "+320-370% Enhanced Damage (varies)",
+            "Ignore Target's Defense",
+            "Adds 180-200 Magic Damage",
+            "Adds 50-200 Fire Damage",
+            "Adds 51-250 Lightning Damage",
+            "Adds 50-200 Cold Damage",
+            "12% Life Stolen Per Hit",
+            "Prevent Monster Heal",
+            "+10 To Strength"
+        ],
+        ladder: false
+    },
+    {
+        name: "Fury",
+        bases: "3 Socket Melee Weapons",
+        runes: ["Jah", "Gul", "Eth"],
+        level: 65,
+        stats: [
+            "40% Increased Attack Speed",
+            "+209% Enhanced Damage",
+            "Ignore Target's Defense",
+            "-25% Target Defense",
+            "20% Bonus To Attack Rating",
+            "6% Life Stolen Per Hit",
+            "33% Deadly Strike",
+            "66% Chance Of Open Wounds",
+            "+5 To Frenzy (Barbarian Only)",
+            "Prevent Monster Heal"
+        ],
+        ladder: false
+    },
+    {
+        name: "Dream",
+        bases: "3 Socket Helms/Shields",
+        runes: ["Io", "Jah", "Pul"],
+        level: 65,
+        stats: [
+            "10% Chance To Cast Level 15 Confuse When Struck",
+            "Level 15 Holy Shock Aura When Equipped",
+            "+20-30% Faster Hit Recovery (varies)",
+            "+30% Enhanced Defense",
+            "+150-220 Defense (varies)",
+            "+10 To Vitality",
+            "Increase Maximum Life 5% (Helms Only)",
+            "+50 To Life (Shields Only)",
+            "+0-61 To Mana (Based On Character Level)",
+            "All Resistances +5-20 (varies)",
+            "12-25% Better Chance of Getting Magic Items (varies)"
+        ],
+        ladder: true
+    },
+    {
+        name: "Ice",
+        bases: "4 Socket Missile Weapons",
+        runes: ["Amn", "Shael", "Jah", "Lo"],
+        level: 65,
+        stats: [
+            "100% Chance To Cast Level 40 Blizzard When You Level-up",
+            "25% Chance To Cast Level 22 Frost Nova On Striking",
+            "Level 18 Holy Freeze Aura When Equipped",
+            "+20% Increased Attack Speed",
+            "+140-210% Enhanced Damage (varies)",
+            "Ignore Target's Defense",
+            "+25-30% To Cold Skill Damage (varies)",
+            "7% Life Stolen Per Hit",
+            "-20% To Enemy Cold Resistance",
+            "20% Deadly Strike",
+            "3-309 Extra Gold From Monsters (Based on Character Level)"
+        ],
+        ladder: true
+    },
+    {
         name: "Eternity",
         bases: "5 Socket Melee Weapons",
         runes: ["Amn", "Ber", "Ist", "Sol", "Sur"],
@@ -176,6 +326,44 @@ let runewords = [
             "Cannot Be Frozen",
             "30% Better Chance Of Getting Magic Items",
             "Level 8 Revive (88 Charges)"
+        ],
+        ladder: false
+    },
+    {
+        name: "Infinity",
+        bases: "4 Socket Polearms",
+        runes: ["Ber", "Mal", "Ber", "Ist"],
+        level: 63,
+        stats: [
+            "50% Chance To Cast Level 20 Chain Lightning When You Kill An Enemy",
+            "Level 12 Conviction Aura When Equipped",
+            "+35% Faster Run/Walk",
+            "+255-325% Enhanced Damage (varies)",
+            "-(45-55)% To Enemy Lightning Resistance (varies)",
+            "40% Chance of Crushing Blow",
+            "Prevent Monster Heal",
+            "0-49 To Vitality (Based on Character Level)",
+            "30% Better Chance of Getting Magic Items",
+            "Level 21 Cyclone Armor (30 Charges)"
+        ],
+        ladder: true
+    },
+    {
+        name: "Chains of Honor",
+        bases: "4 Socket Body Armor",
+        runes: ["Dol", "Um", "Ber", "Ist"],
+        level: 63,
+        stats: [
+            "+2 To All Skills",
+            "+200% Damage To Demons",
+            "+100% Damage To Undead",
+            "8% Life Stolen Per Hit",
+            "+70% Enhanced Defense",
+            "+20 To Strength",
+            "Replenish Life +7",
+            "All Resistances +65",
+            "Damage Reduced By 8%",
+            "25% Better Chance of Getting Magic Items"
         ],
         ladder: false
     },
